6bbfa637dd68623cb83c98979f070aae5faea12e
Jorge Gonzalez <jorgeejgonzalez@gmail.com>
2015-04-01 21:47:46 -0430

    Missing integer aplicando for each, 100%

--- MissingInteger/src/Solution.java
+++ MissingInteger/src/Solution.java
@@ -1,17 +1,14 @@
 import java.util.Arrays;
-import java.util.HashSet;
-
 
 public class Solution {
 	
-	public int solution(int[] A) {
-		
+	public int solution(int[] A) {		
 		int missing = 1;
 		
 		Arrays.sort(A);
 		
-		for(int i=0;i<A.length;++i){			
-			if(A[i]==missing){
+		for(int i : A){			
+			if(i==missing){
 				missing++;
 			}
 		}
